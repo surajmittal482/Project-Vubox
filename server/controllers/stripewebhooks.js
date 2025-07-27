1abae32163f1d445a6756cc07a0971aff576cde5
suraj123 <surajmittal432@gmail.com>
2025-07-28 03:00:32 +0530

    change

--- server/controllers/stripewebhooks.js
+++ server/controllers/stripewebhooks.js
@@ -1,5 +1,6 @@
 import stripe from "stripe";
 import Booking from "../models/Booking.js";
+import { inngest } from "../inngest/index.js";
 
 export const stripeWebhooks = async (request, response) => {
   console.log("Received Stripe webhook event");
